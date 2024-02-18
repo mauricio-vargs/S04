1699ff2c680f54eebacab56853d709c592b63e2b
Ivaav <mauricio.v27@outlook.com>
2024-02-18 16:20:39 -0300

    Correção da condição else if no exercicio 10

--- ex10/ex10.js
+++ ex10/ex10.js
@@ -10,7 +10,7 @@ function setAge(){
 function verifyAge(age){
     if(age >= 18){
         return `Você é maior de idade!`
-    }else if(age <= 18 && age >= 0){
+    }else if(age < 18 && age >= 0){
         return `Você é menor de idade!`
     }else {
         return `Insira um valor numérico e positivo!`
