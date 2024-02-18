6a80473b0bbd57129a4683b4041bf2f387a07bf6
Ivaav <mauricio.v27@outlook.com>
2024-02-18 16:19:08 -0300

    Pequena alteração no exercício 8

--- ex08/ex08.js
+++ ex08/ex08.js
@@ -1,5 +1,9 @@
 function parOuImpar(num){
-    if(num % 2 === 0){
+    if(num === 0){
+        return `O número passado é 0`
+    }
+    
+    else if(num % 2 === 0){
         return `O numero ${num} é par`
     }
     else{
@@ -7,5 +11,6 @@ function parOuImpar(num){
     }
 }
 
+console.log(parOuImpar(0))
 console.log(parOuImpar(5))
 console.log(parOuImpar(4))
